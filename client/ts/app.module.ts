a032aa8f6f95cda7da5d1ee5a405629517d0e27f
Josh <jspoole04@gmail.com>
2016-01-05 21:59:10 -0500

    Changed app name and added new ui-router config.

--- client/ts/app.module.ts
+++ client/ts/app.module.ts
@@ -1,9 +1,18 @@
 /// <reference path="../../typings/tsd.d.ts" />
 
 module favoriteScrapper {
+	export function Config($stateProvider: ng.ui.IStateProvider, $urlRouterProvider: ng.ui.IUrlRouterProvider) {
+		$urlRouterProvider.otherwise('/');
 
+		$stateProvider.state('home', {
+			url: '/',
+			templateUrl: '/partials/home.html'
+		});
+	}
 }
 
 (() => {
-	angular.module('favoriteScrapper', ['ui.router', 'reddit']);
+	let app = angular.module('favoriteScrapper', ['ui.router', 'reddit']);
+
+	app.config(['$stateProvider', '$urlRouterProvider', favoriteScrapper.Config]);
 })();
